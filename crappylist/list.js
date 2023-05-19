c5229d4212efa2d681a8b57b08e677d06dd10b37
A. popescu <i500546@fhict.nl>
2023-05-19 13:04:17 +0200

    a lot of changes

---
+++ crappylist/list.js
@@ -0,0 +1,71 @@
+fetch('items.json')
+ .then(response => response.json())
+ .then(data => initList(data))
+
+
+function inputClicked(){
+    input = document.getElementById("todoInput");
+    addLi(input.value);
+
+    //trying to save changes to the file, doesnt work
+    todoList = document.getElementById("todoList");
+    console.log(todoList);
+
+    var values = [];
+
+    for (var item of todoList.children) {
+        // console.log(item);
+        if (item.nodeName == "LI") {
+
+            var value = item.childNodes[3].innerHTML;
+            values.push({"value": value});
+        }
+    }
+    console.log(values)
+
+    fetch("/test.json", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        
+        },
+        body: JSON.stringify(values)
+    });
+
+}
+
+
+function initList(data){
+    for(var item of data){
+        addLi(item.value);
+    }   
+ }
+ 
+
+function addLi(value){
+    todoList = document.getElementById("todoList");
+
+    var randomValue = Math.floor(Math.random() * 100000000);
+    var li = `
+        <li id="${randomValue}">
+            <button id="delete" onclick="deleteLi('${randomValue}')">
+                Del
+            </button>
+            <label>${value}</label>
+        </li>
+    
+    `
+    todoList.innerHTML += li;
+}
+
+function deleteLi(id){
+    var li = document.getElementById(id);
+    li.parentNode.removeChild(li);
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
